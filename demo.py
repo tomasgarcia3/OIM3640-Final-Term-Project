02b5e2f9b5aacfa8a0efcb6095d61d9c48b85bdc
tomasgarcia3 <tgarcia1@babson.edu>
2022-04-29 12:04:10 -0400

    demo

---
+++ demo.py
@@ -0,0 +1,21 @@
+# Test Code
+import ccxt
+
+# exchange = ccxt.binance()
+print(ccxt.exchanges)
+
+# exchange = ccxt.okcoinusd () # default id
+# okcoin1 = ccxt.okcoinusd ({ 'id': 'okcoin1' })
+# okcoin2 = ccxt.okcoinusd ({ 'id': 'okcoin2' })
+# id = 'btcchina'
+# btcchina = eval ('ccxt.%s ()' % id)
+# coinbasepro = getattr (ccxt, 'coinbasepro') ()
+
+# from variable id
+# exchange_id = 'binance'
+# exchange_class = getattr(ccxt, exchange_id)
+# exchange = exchange_class({
+#     'apiKey': 'YOUR_API_KEY',
+#     'secret': 'YOUR_SECRET',
+# })
+
